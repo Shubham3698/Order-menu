2365ef0a5f0b62a75fcf5c3bda9ca2f996582bd8
Shubham Pandey <Pandey0shubham3698@gmail.com>
2024-11-07 01:32:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CardComponent from './component/Cardcomo'
+
+function App() {
+  return (
+    <div >
+    <CardComponent/>
+    {/* <br />
+    <CardComponent/>
+    <br />
+    <CardComponent/>
+    <br />
+    <CardComponent/> */}
+    </div>
+  )
+}
+
+export default App
