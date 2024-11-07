0c021db202643e9d0094e04f8933ad9f4f699fba
Shubham Pandey <Pandey0shubham3698@gmail.com>
2024-11-07 13:21:28 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ProductCard from './component/Cardcomo';  // Assuming Cardcomo is the file with ProductCard
-import Hero from './component/Hero.jsx';  // Assuming Cardcomo is the file with ProductCard
+// App.jsx
+import React, { useState } from 'react';
+import ProductCard from './component/Cardcomo';
+import Hero from './component/Hero';
 
 function App() {
-  // Define product data
   const products = [
     {
       name: 'Honey Jar 1',
@@ -11,7 +11,7 @@ function App() {
       rating: 4.5,
       originalPrice: 500,
       discountedPrice: 450,
-      phoneNumber: '7080981033',
+      phoneNumber: '919876543210',
     },
     {
       name: 'Honey Jar 2',
@@ -19,7 +19,7 @@ function App() {
       rating: 4.0,
       originalPrice: 600,
       discountedPrice: 550,
-      phoneNumber: '7080981033',
+      phoneNumber: '919876543210',
     },
     {
       name: 'Honey Jar 3',
@@ -27,42 +27,40 @@ function App() {
       rating: 3.5,
       originalPrice: 450,
       discountedPrice: 400,
-      phoneNumber: '7080981033',
+      phoneNumber: '919876543210',
     },
-    {
-      name: 'Honey Jar 4',
-      image: 'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908__340.jpg',
-      rating: 4.8,
-      originalPrice: 700,
-      discountedPrice: 650,
-      phoneNumber: '7080981033',
-    },
-    {
-      name: 'Honey Jar 5',
-      image: 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__340.jpg',
-      rating: 4.8,
-      originalPrice: 700,
-      discountedPrice: 650,
-      phoneNumber: '7080981033',
-    }
   ];
+
+  const [currentProductIndex, setCurrentProductIndex] = useState(0);
+
+  const goToNextProduct = () => {
+    setCurrentProductIndex((currentProductIndex + 1) % products.length);
+  };
+
+  const goToPreviousProduct = () => {
+    setCurrentProductIndex((currentProductIndex - 1 + products.length) % products.length);
+  };
 
   return (
     <div>
-      <Hero/>
-      {products.map((product, index) => (
-        <div key={index}>
-          <ProductCard
-            name={product.name}
-            image={product.image}
-            rating={product.rating}
-            originalPrice={product.originalPrice}
-            discountedPrice={product.discountedPrice}
-            phoneNumber={product.phoneNumber}
-          />
-          <br />
-        </div>
-      ))}
+      <Hero />
+      <br />
+      <ProductCard
+        {...products[currentProductIndex]}
+        goToNextProduct={goToNextProduct}
+        goToPreviousProduct={goToPreviousProduct}
+      />
+      br
+      <ProductCard
+        {...products[currentProductIndex]}
+        goToNextProduct={goToNextProduct}
+        goToPreviousProduct={goToPreviousProduct}
+      />
+      <ProductCard
+        {...products[currentProductIndex]}
+        goToNextProduct={goToNextProduct}
+        goToPreviousProduct={goToPreviousProduct}
+      />
     </div>
   );
 }
